1c140cfb65151a3b0cbedf2aa3dc7e0d3c994e5a
Juan Carlos <jchernandez827@gmail.com>
2022-06-22 15:23:30 -0500

    Add meme image ans styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,9 @@ function Main() {
   return (
     <div className="main">
       <Form />
+      <span className="top-text">SHUT UP</span>
+      <span className="bottom-text">AND TAKE MY MONEY</span>
+      <img className="meme-img" src="img/meme.png" alt="meme" />
     </div>
   );
 }
